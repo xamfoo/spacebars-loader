82467e34ebbe56300255b81092b5c3018d4ade84
xamfoo <xamfoo@all.repo>
2015-07-17 22:01:38 +0800

    Update loader export
    - Make loader cacheable
    - Pass query options to html scanner

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+var loaderUtils = require("loader-utils");
 var html_scanner = require('./templating/html_scanner.js');
 var doHTMLScanning = require('./templating/compile-templates.js');
 
-module.exports = function(source) {
-  return doHTMLScanning.call(this, source, html_scanner);
+module.exports = function(source, sourceMap) {
+  if (this.cacheable) this.cacheable();
+
+  var options = loaderUtils.parseQuery(this.query);
+  return doHTMLScanning.call(this, source, html_scanner, options);
 };
